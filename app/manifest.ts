13f79f25120800613505612e86b0dfb8a6dacce5
Luis Altamirano <seaintco@gmail.com>
2026-06-09 14:40:01 -0500

    Add Purity logo and PWA icon

---
+++ app/manifest.ts
@@ -0,0 +1,19 @@
+﻿import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Purity Of Heart",
+    short_name: "Purity",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fbf8f3",
+    theme_color: "#241f1b",
+    icons: [
+      {
+        src: "/purity-logo.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
